fbe112b74e9661cc288474fea6b8eb394c988fc6
booleanhunter <ashwin01h@gmail.com>
2016-10-09 11:11:32 +0530

    Changes: Restructuring files Removing redundant components and code Removed requirejs from Node code

---
+++ configs/server-configs/config.js
@@ -0,0 +1,16 @@
+/* @author booleanhunter
+ * @details Configuration file 
+ */
+var config = {
+    production: {
+        server_port1: 443,
+        server_port2: 80
+    },
+    development: {
+        server_port1: 8000,
+        server_port2: 8080,
+        googleBackEndPlacesKey: "AIzaSyBE1zF3esVLiworvuhyNkC9hAi4t1AFmlU"
+    }
+}
+
+exports.config = config
